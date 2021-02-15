75a3c9e6ddd0fef2c1832c690d6143b9d51ee9aa
autsadawut4869 <78350629+autsadawut4869@users.noreply.github.com>
2021-02-15 18:00:39 +0700

    edit

--- lab3_1.c
+++ lab3_1.c
@@ -2,13 +2,26 @@
 int main()
 {
     int play[5][4];
-    int i,j;
+    int i,j,score[5],bs=0,people=0;
     for(i=0;i<5;i++)
     {
         for(j=0;j<4;j++){
             scanf("%d",&play[i][j]);
         }
     }
+    for(i=0;i<5;i++)
+    {
+        score[i]=0;
+        for(j=0;j<4;j++){
+            score[i] += play[i][j];
+            if(bs<score[i]){
+                bs = score[i];
+                people = i;
+            }
+        }
+    }
+
+    printf("%d %d",people,bs);
 
     return 0;
 }
