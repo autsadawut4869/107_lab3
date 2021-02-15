4a5ce2d4c190770873f3e72ad759435e33420d0b
autsadawut4869 <78350629+autsadawut4869@users.noreply.github.com>
2021-02-15 17:49:18 +0700

    edit

---
+++ lab3_1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int play[5][4];
+    int i,j;
+    for(i=0;i<5;i++)
+    {
+        for(j=0;j<4;j++){
+            scanf("%d",&play[i][j]);
+        }
+    }
+
+    return 0;
+}
