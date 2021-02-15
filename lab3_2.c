b9c43324354d9fbfe581392c66b7c9393a4c2bd5
autsadawut4869 <78350629+autsadawut4869@users.noreply.github.com>
2021-02-15 18:29:16 +0700

    edit

--- lab3_2.c
+++ lab3_2.c
@@ -1,6 +1,20 @@
 #include<stdio.h>
 int main()
 {
+    long long int x,i;
+    scanf("%lld",&x);
+
+    if(x%2==0){
+        if(x==2){
+           printf("%d",x);
+        }
+        else{
+           printf("%d",x-1);
+        }
+    }
+    else{
+        printf("%d",x);
+    }
 
     return 0;
 }
